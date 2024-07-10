b5c9e514c04de3ed6f074ec5d350ec6d00e42f0c
Aymen Mohsni <aymenmohsnitn@gmail.com>
2024-07-10 11:26:26 +0100

    updated services, added valotech page

--- script.js
+++ script.js
@@ -97,23 +97,68 @@ if (formContact) {
 }
 
 // search 
-const search = document.getElementById("search");
+// const search = document.getElementById("search");
 
-search.addEventListener("keyup", (e) => {
-   const searchValue = e.target.value.toLowerCase();
-   const courseCard = document.querySelectorAll(".course-card");
+// search.addEventListener("keyup", (e) => {
+//    const searchValue = e.target.value.toLowerCase();
+//    const courseCard = document.querySelectorAll(".course-card");
 
-   courseCard.forEach((card) => {
-      const title = card.querySelector(".course-description h1").textContent.toLowerCase();
+//    courseCard.forEach((card) => {
+//       const title = card.querySelector(".course-description h1").textContent.toLowerCase();
 
-      if (title.includes(searchValue)) {
-         card.style.display = "flex";
-      } else {
-         card.style.display = "none";
-      }
-   });
-});
+//       if (title.includes(searchValue)) {
+//          card.style.display = "flex";
+//       } else {
+//          card.style.display = "none";
+//       }
+//    });
+// });
 
+
+// Filter elements in services
+filterSelection("all")
+        function filterSelection(c) {
+          var x, i;
+          x = document.getElementsByClassName("filterDiv");
+          for (i = 0; i < x.length; i++) {
+            w3RemoveClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+          }
+        }
+        
+        function w3AddClass(element, name) {
+          var i, arr1, arr2;
+          arr1 = element.className.split(" ");
+          arr2 = name.split(" ");
+          for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+          }
+        }
+        
+        function w3RemoveClass(element, name) {
+          var i, arr1, arr2;
+          arr1 = element.className.split(" ");
+          arr2 = name.split(" ");
+          for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+              arr1.splice(arr1.indexOf(arr2[i]), 1);     
+            }
+          }
+          element.className = arr1.join(" ");
+        }
+        
+        // Add active class to the current button (highlight it)
+        var btnContainer = document.getElementById("myBtnContainer");
+        var btns = btnContainer.getElementsByClassName("btn");
+        for (var i = 0; i < btns.length; i++) {
+          btns[i].addEventListener("click", function(){
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+          });
+        }
+
+// collapsible
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
